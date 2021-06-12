468fbdbab085a25e3e3bf4e90dff1625e088be3f
BrianKel522 <briankel522@gmail.com>
2021-06-12 14:55:06 -0400

    Joey basically did all of level 8 so i will jokingly blame him for everything so he is huge newblet

---
+++ BLUEbutton l7.gd
@@ -0,0 +1,37 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "RobotO":
+		body.dead = true
+		yield(get_tree().create_timer(1),"timeout")
+		SceneChanger.change_Scene("res://Level7.tscn")
+		SceneChanger.maddness += 1
+	if body.name == "RobotB":
+		body.dead = true
+		yield(get_tree().create_timer(1),"timeout")
+		SceneChanger.change_Scene("res://Level7.tscn")
+		SceneChanger.maddness += 1
+
+
+func _on_Button_body_entered(body):
+	if body.name == "RobotO":
+		get_node("/root/Level7/tiles1/side1/Tile7").frame = 15
+		$AnimationPlayer.play("move")
+	if body.name == "RobotB":
+		get_node("/root/Level7/tiles1/side1/Tile7").frame = 15
+		$AnimationPlayer.play("move")
+		
+
+func _on_Button_body_exited(body):
+	if body.name == "RobotO":
+		get_node("/root/Level7/tiles1/side1/Tile7").frame = 14
+		$AnimationPlayer.play_backwards("move")
+	if body.name == "RobotB":
+		get_node("/root/Level7/tiles1/side1/Tile7").frame = 14
+		$AnimationPlayer.play_backwards("move")
---
+++ RedButtonBox l7.gd
@@ -0,0 +1,32 @@
+extends Sprite
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "RobotO":
+		body.dead = true
+		yield(get_tree().create_timer(1),"timeout")
+		SceneChanger.change_Scene("res://Level7.tscn")
+		SceneChanger.maddness += 1
+	if body.name == "RobotB":
+		body.dead = true
+		yield(get_tree().create_timer(1),"timeout")
+		SceneChanger.change_Scene("res://Level7.tscn")
+		SceneChanger.maddness += 1
+
+
+func _on_Button_body_entered(body):
+	if body.name == "RobotO":
+		get_node("/root/Level7/tiles2/side1/CTile3").frame = 7
+		$AnimationPlayer.play("move")
+	if body.name == "RobotB":
+		get_node("/root/Level7/tiles2/side1/CTile3").frame = 7
+		$AnimationPlayer.play("move")
+		
+
+func _on_Button_body_exited(body):
+	if body.name == "RobotO":
+		get_node("/root/Level7/tiles2/side1/CTile3").frame = 6
+		$AnimationPlayer.play_backwards("move")
+	if body.name == "RobotB":
+		get_node("/root/Level7/tiles2/side1/CTile3").frame = 6
+		$AnimationPlayer.play_backwards("move")
